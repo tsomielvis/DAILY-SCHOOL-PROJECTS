55672f5af18cd0e2a54e0cbcb48d0ff1927ab5b8
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-06-20 12:07:44 +0300

    20-june-2024

---
+++ looping.js
@@ -0,0 +1,45 @@
+// for (let step =0; step <=20; step +=1){
+//   console.log(step)
+// }
+
+////// while loop
+//variable
+
+//while (condition) {
+
+    // increment
+    // }
+
+    // let step = 10
+
+    // while (step <20) {
+
+    //     console.log(step)
+    //     // step -== 1
+    //     step++
+    //     step = step - 1
+    // let step = 0
+      
+    // do {
+    //    console.log(step)
+    // step++
+    //
+    //
+    // } while (step <= 20)
+
+    // A =100 -70
+    // B =70 -40
+    // C =40 - 0
+
+    let marks = 75;
+
+    if (marks <=100 && marks >=70) {
+    console.log('You Got A');
+    } else if(marks <=69 && marks >= 40) {
+      console.log('You Got B')
+    }else if (marks < 40 && marks >= 0) {
+        console.log ('You Got c')
+    }
+    else {
+      console.log('Out of Range')    
+    }
